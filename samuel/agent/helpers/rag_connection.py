5fa723f318dbf4786982c23f04e6e1ac70832437
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2025-03-31 22:21:25 +0000

    refactor: update get_rag_context function to use POST request and restructure response handling

--- samuel/agent/helpers/rag_connection.py
+++ samuel/agent/helpers/rag_connection.py
@@ -2,13 +2,14 @@
 # 23 01 2025
 
 import requests
-from ..setup import RAG_URL, RAG_COLLECTION
+from ..setup import RAG_URL
 
-def get_rag_context(company_id: str, prompt: str):
-    context = requests.get(f"{RAG_URL}/get_context", params={
-            "database": company_id,
-            "collection": RAG_COLLECTION,
-            "query": prompt
-        })
-
-    return context.json()["body"]
+def get_rag_context(organization_id: str, prompt: str):
+    url = f"{RAG_URL}/search?organization_id={organization_id}&prompt={prompt}"
+    context = requests.post(url)
+    
+    context_list = []
+    for chunk in context.json():
+        context_list.append(chunk["fields"]["content"])
+    
+    return "\n".join(context_list)
